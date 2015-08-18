cc79697e919e216ff424cc9f6fcb906f7e396c15
Orthoc <kuuinshi@gmail.com>
2015-08-18 12:16:17 +0100

    Running logo/spiral.py

--- logo/spiral.py
+++ logo/spiral.py
@@ -10,11 +10,3 @@ def spiral(size):
   spiral(size + 10)
   
 spiral(0)
-
-def polygon(edges, size):
-  angle = 360.0 / edges
-  for i in range(0, edges):
-    move(size)
-    turn(angle)
-    
-polygon(4,100)

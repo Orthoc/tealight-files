7a02a8cf1cdf78dfa0b762f11695b9df471895bb
Orthoc <kuuinshi@gmail.com>
2015-08-18 12:13:31 +0100

    Running logo/spiral.py

--- logo/spiral.py
+++ logo/spiral.py
@@ -7,16 +7,7 @@ def spiral(size):
   
   move(size)
   turn(90)
-  spiral(size + 15)
+  spiral(size + 10)
   
 spiral(0)
 
-def spiral2(size):
-  if size > 300:
-    return
-  
-  move (size)
-  turn (90)
-  spiral(size + 15)
-
-spiral (1)

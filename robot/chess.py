d1fa1f0d1a708d1e1b6e37d73c19f1cac2aedcbf
Orthoc <kuuinshi@gmail.com>
2015-08-18 14:51:43 +0100

    Running robot/chess.py

--- robot/chess.py
+++ robot/chess.py
@@ -7,6 +7,14 @@ from tealight.robot import (move,
                             right_side)
 
 # Add your code here
-distance = 31
+distance = 32
 for n in range(0, distance):
     move()
+
+turn(1)
+
+distance = 32
+for n in range(0, distance):
+    move()
+
+turn(1)

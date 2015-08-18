2bcf4e952555c6ccbbe33b32ac3b23fa0f75d815
Orthoc <kuuinshi@gmail.com>
2015-08-18 12:23:55 +0100

    Running logo/chessboard.py

--- logo/chessboard.py
+++ logo/chessboard.py
@@ -1,6 +1,15 @@
 print "A new file!"
 from tealight.logo import move, turn, color
 
+def polygon(edges, size):
+  angle = 360.0 / edges
+  for i in range(0, edges):
+    move(size)
+    turn(angle)
+    
+polygon(4,100)
+
+
 move(8)
 turn(90)
 move(8)

7c22124feee7db0eb11a7647e1639214a7074be4
Orthoc <kuuinshi@gmail.com>
2015-08-20 10:49:03 +0100

    Running art/untitled.py

--- art/untitled.py
+++ art/untitled.py
@@ -2,4 +2,4 @@ print "A new file!"
 from tealight.art import (color, line, spot, circle, box, image, text, background)
 from tealight import *
 def handle_mousemove(x,y):
-  print 'The mouse moved to ' + x + ',' + y 
+  print 'The mouse moved to ' + x + ',' + y

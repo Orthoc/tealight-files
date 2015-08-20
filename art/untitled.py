c2c65a02dd3cac8223d97e3157cc00088bb02435
Orthoc <kuuinshi@gmail.com>
2015-08-20 12:07:47 +0100

    Running art/untitled.py

--- art/untitled.py
+++ art/untitled.py
@@ -2,7 +2,17 @@ print "A new file!"
 from tealight.art import (color, line, spot, circle, box, image, text, background)
 from tealight import *
 
+def DrawMark(x,y)
+  
 
 def handle_mousedown(x,y):
-  print x
+   
 
+if button == 'left':
+  if input() is in range(0, )
+    del square
+
+    
+def emptysquare():
+  color(white)
+  box(x, y, 30, 30)
